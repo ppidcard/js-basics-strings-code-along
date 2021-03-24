38cf5d0809278efb71b8696978ce7b8710f43860
ppidcard <tvxqlikei@live.cn>
2021-03-24 21:01:22 +1100

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your code in this file!
+
+let currentUser = "Alexis";
+
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const upperCaseCurrentUser = currentUser.toUpperCase();
+
+let excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${upperCaseCurrentUser}!`;
+
+const firstInitial = currentUser[0];
+
+let shortGreeting = `Welcome, ${firstInitial}!`;
+
+
+
+const restOfName = currentUser.slice(1);
+
+console.log(firstInitial);
+ 
